f38c0677a7b5380759b96fad4e0430e602844680
Ana Prince <msanaprince@gmail.com>
2020-03-08 17:47:52 +0000

    Add check-filter component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import './style.scss';
+import genres from './util/genres';
+
 new Vue({
 	el: '#app',
 	components: {
@@ -18,9 +20,22 @@ new Vue({
 			}
 		},
 		'movie-filter': {
+			data() {
+				return {
+					genres
+				}
+			},
 			template: `<div id="movie-filter">
 							<h2>Filter results</h2>
-						</div>`
+							<div class="filter-group">
+								<check-filter v-for="genre in genres"></check-filter>
+							</div>
+						</div>`,
+			components: {
+				'check-filter': {
+					template: `<div>Filter</div>`
+				}
+			}
 		}
 	}
 });
