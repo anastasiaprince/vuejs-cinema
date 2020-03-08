5741c04a556f3e4e4771cf5e500daf3e05871e17
Ana Prince <msanaprince@gmail.com>
2020-03-08 17:37:58 +0000

    Add movie list and filter results

--- src/main.js
+++ src/main.js
@@ -2,7 +2,25 @@ import Vue from 'vue';
 import './style.scss';
 new Vue({
 	el: '#app',
-	data: {
-		msg: 'Howdy'
+	components: {
+		'movie-list': {
+			template: `<div id="movie-list">
+							<div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+						</div>`,
+			data() {
+				return {
+					movies: [
+						{ title: 'A League of Their Own' },
+						{ title: 'Fried Green Tomatoes' },
+						{ title: 'Thelma & Louise' }
+					]
+				};
+			}
+		},
+		'movie-filter': {
+			template: `<div id="movie-filter">
+							<h2>Filter results</h2>
+						</div>`
+		}
 	}
 });
